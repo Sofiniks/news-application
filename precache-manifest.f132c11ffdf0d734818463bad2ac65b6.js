3418b4fec0ba118cfcff1cb7b8c8a7fb5bbcacae
Sofija <sofija.nikitina@inbox.lv>
2020-06-11 09:46:13 +0300

    Updates

---
+++ precache-manifest.f132c11ffdf0d734818463bad2ac65b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818359131276721a3ae9bda06c2ddb92",
+    "url": "/news-application/index.html"
+  },
+  {
+    "revision": "d74b48800b6b92205d5f",
+    "url": "/news-application/static/css/main.94919180.chunk.css"
+  },
+  {
+    "revision": "4416da5816f958711a4d",
+    "url": "/news-application/static/js/2.87f6dac6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-application/static/js/2.87f6dac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74b48800b6b92205d5f",
+    "url": "/news-application/static/js/main.fc51d032.chunk.js"
+  },
+  {
+    "revision": "3f0948b5855671b5a320",
+    "url": "/news-application/static/js/runtime-main.86e1ad1e.js"
+  },
+  {
+    "revision": "20acadc752a229069b8b73ea2679ba33",
+    "url": "/news-application/static/media/E732151C-96E3-41D1-B60D-52CCD4963757.20acadc7.jpeg"
+  },
+  {
+    "revision": "386c8ff06aaa9d3ea14528a5703b6ec5",
+    "url": "/news-application/static/media/ofont.ru_Proxima Nova.386c8ff0.ttf"
+  }
+]);
